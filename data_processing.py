0ad1fcf165087bc16416488bd8db8cfb2cbdd0dc
shrutssss <shrutiv.jahagirdar@gmail.com>
2026-07-01 00:03:21 +0530

    data processing: create formatted data

--- data_processing.py
+++ data_processing.py
@@ -1,18 +1,25 @@
+import os
 import pandas as pd
+import csv
 
-# Convert data files to dataframes
-df1 = pd.read_csv(r"C:\Users\jahag\OneDrive\Desktop\repos\quantium-starter-repo\data\daily_sales_data_0.csv")
-df2 = pd.read_csv(r"C:\Users\jahag\OneDrive\Desktop\repos\quantium-starter-repo\data\daily_sales_data_1.csv")
-df3 = pd.read_csv(r"C:\Users\jahag\OneDrive\Desktop\repos\quantium-starter-repo\data\daily_sales_data_2.csv")
+DATA_DIR = "./data"
 
-# Combine all dataframes into one single dataframe
-combined_df = pd.concat([df1,df2,df3], ignore_index=True)
+dfs = []
 
-# Filter combined_df having product as 'pink morsel'
-filter_by_pink_morsel_df = combined_df[combined_df["product"] == "pink morsel"]
+for file in os.listdir(DATA_DIR):
+    dfs.append(pd.read_csv(os.path.join(DATA_DIR,file)))
+    
+df = pd.concat(dfs)
 
-final_df = filter_by_pink_morsel_df.drop(columns=["product"])
-final_df["sales"] = final_df["price"] * final_df["quantity"]
-final_df = final_df.drop(columns=["price", "quantity"])
+print(f"After concat: \n {df.info()}")
 
-print(final_df.info())
+df = df[df['product'] == 'pink morsel']
+
+df["price"] = df["price"].replace(r"[$]", "", regex = True).astype(float)
+
+
+df["sales"] = df["price"] * df["quantity"]
+
+df = df.loc[:, ["sales", "date", "product"]]
+
+df.to_csv("formatted_data.csv", index = False)
